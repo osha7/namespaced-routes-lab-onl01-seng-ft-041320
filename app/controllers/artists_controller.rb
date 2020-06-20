7835d9300dd2f266535cccabf64b8ece544cdbb6
osha7 <ogroetz@gmail.com>
2020-06-20 10:09:01 -0600

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,7 +8,12 @@ class ArtistsController < ApplicationController
   end
 
   def new
-    @artist = Artist.new
+    preference = Preference.firt
+    if preference.all_create_artists == true
+      @artist = Artist.new
+    else
+      redirect_to artists_path
+    end
   end
 
   def create
